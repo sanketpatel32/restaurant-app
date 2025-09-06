5580b6bb0a281439e63bf58c0369006dc3356e9a
Sanket Patel <sanpatel323@gmail.com>
2025-09-06 16:34:07 +0530

    Added states

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,19 @@ import './App.css'
 import Meals from './components/Meals/Meals'
 import Header from './components/Layout/Header'
 import Cart from './components/Cart/Cart'
+import { useState } from 'react'
 function App() {
+  const[cartIsShown,setCartIsShown] = useState(false)
+  const showCartHandler = ()=>{
+    setCartIsShown(true)
+  }
+  const hideCartHandler = ()=>{
+    setCartIsShown(false)
+  }
   return (
     <>
-  <Cart/>
-    <Header/>
+  {cartIsShown && <Cart onClose = {hideCartHandler}/>}
+    <Header onShowCart = {showCartHandler}  onHideCart = {hideCartHandler}/>
     <Meals/>
     </>
   )
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import classes from './Cart.module.css'
 import Modal from '../UI/Modal';
-const Cart = () => {
+const Cart = (props) => {
 
 const cartItems = (
     <ul>
@@ -12,7 +12,7 @@ const cartItems = (
 );
 
     return (
-        <Modal>
+        <Modal onClose={props.onClose}>
 
             {cartItems}
             <div className={classes.total}>
@@ -21,7 +21,7 @@ const cartItems = (
             </div>
 
             <div className={classes.actions}>
-                <button className={classes['button--alt']}>Close</button>
+                <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
                 <button className={classes.button}>Order</button>
 
             </div>
